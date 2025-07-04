32a682cfc093b12ba836a6615ff46c6f3eb8d59f
742942Ayan <pgdisha9520@gmail.com>
2025-07-04 14:12:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// Express Server Entry Point
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('AdGlow Backend is live!');
+});
+
+// Example route
+// app.use('/api/kyc', require('./routes/kycRoute'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
