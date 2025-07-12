4f3955388c0113a747155eeca49b2bd8acfc24e8
742942Ayan <pgdisha9520@gmail.com>
2025-07-13 00:05:50 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,11 @@
 
 const express = require("express");
 const router = express.Router();
-const { registerUser, verifyOtp } = require("../controllers/authController");
+const { register, verifyOtp } = require("../controllers/authController");
 
 // @route   POST /api/auth/register
 // @desc    Register user and send OTP
-router.post("/register", registerUser);
+router.post("/register", register);
 
 // @route   POST /api/auth/verify
 // @desc    Verify OTP
