0f0ee36d571d67604c439917780b5bbb2f8962c5
742942Ayan <pgdisha9520@gmail.com>
2025-07-15 14:53:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+dotenv.config(); // ✅ Must be first!
 const connectDB = require("./config/db");
 
 dotenv.config();
