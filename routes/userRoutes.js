266534dfcb096d6e4288d9b648ec6585671a0e69
742942Ayan <pgdisha9520@gmail.com>
2025-07-15 21:59:52 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,15 @@
 // User routes: KYC, Profile
 const express = require("express");
 const router = express.Router();
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+const { getUserProfile } = require("../controllers/userController");
+
+router.get("/profile", authMiddleware, getUserProfile);
+
+module.exports = router;
+
 
 // @route POST /api/user/kyc
 // @desc Submit KYC form
