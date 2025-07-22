0f2335b2aada9a666bd51337b4c7fc8da700adea
742942Ayan <pgdisha9520@gmail.com>
2025-07-22 23:21:10 +0530

    Update withdrawalRoutes.js

--- routes/withdrawalRoutes.js
+++ routes/withdrawalRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 const Withdrawal = require("../models/Withdrawal");
 const User = require("../models/User");
 
